e1bdb64fffe0dc53b2af0924635d0e14909136ff
RavanBayramov1 <ravanvb-ab108@code.edu.az>
2026-04-28 17:41:33 +0400

    Second Exam Project

---
+++ ExamSystem/DTOs/ResultDtos/UpdateResultDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamSystem.DTOs.ResultDtos;
+
+public class UpdateResultDto
+{
+    [Required(ErrorMessage = "Status boş ola bilməz.")]
+    [RegularExpression("pending|graded", ErrorMessage = "Status pending və ya graded olmalıdır.")]
+    public string Status { get; set; } = string.Empty;
+}
--- ExamSystem/obj/Debug/net8.0/ExamSystem.AssemblyInfo.cs
+++ ExamSystem/obj/Debug/net8.0/ExamSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2dd8ab3669ab32063ceadd16b6d1624c3403adb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
